35b354f2e46e47e97f5135813e8830f8c59f4495
Qentes01 <89841734+Qentes01@users.noreply.github.com>
2021-09-08 08:36:51 +0300

    Update Program.cs

--- Greetings/Program.cs
+++ Greetings/Program.cs
@@ -6,7 +6,20 @@ namespace GreenTigs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //programm küsib kasutaja nime
+            //programm tervitab kasutajat nimepidi
+            Console.WriteLine("Mis su nimi on?");
+
+            string UserName = Console.ReadLine();
+
+            //Console.WriteLine("Hello, " + UserName);
+
+            //string interpolation
+
+            //{} - altGr + 7 / altGr + 0
+            Console.WriteLine($"Hello, {UserName}"); 
+
+            Console.Read();
         }
     }
 }
